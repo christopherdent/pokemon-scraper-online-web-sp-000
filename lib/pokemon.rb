c6e20f77cd3cc8e3a9ed7cff16b47b3bd0c4fbc8
Christopher Dent <christopherdent01@gmail.com>
2019-10-31 21:53:41 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -29,7 +29,7 @@ class Pokemon
         LIMIT 1 
       SQL
       
-      db.execute(sql, id).map {|row| self.new(row[0], row[1], row[2], db)}.first  
+      db.execute(sql, id).map {|row| self.new(id: row[0], name: row[1], type: row[2], db)}.first  
   end 
     
   
