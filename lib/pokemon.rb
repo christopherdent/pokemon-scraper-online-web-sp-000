4c0b75fd7aa1b2cfc6b35611ea4990298217ba72
Christopher Dent <christopherdent01@gmail.com>
2019-10-31 07:13:22 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,40 @@
+require 'pry'
+
+
 class Pokemon
+  attr_accessor :id, :name, :type, :db 
+
+  def initialize(id: nil, name:, type:)
+    @id = id
+    @name = name 
+    @type = type 
+    @db = db 
+
+  end 
+
+  def self.save(name, type, db)
+    sql = <<-SQL 
+      INSERT INTO pokemon (name, type) 
+      VALUES (?, ?)
+    SQL
+    db.execute(sql, name, type)
+     
+  end 
+  
+  
+  def self.find(id, db)   #find by ID number and return an Object 
+    sql = <<-SQL 
+      SELECT * FROM pokemon
+        WHERE id = ?;
+      SQL
+      binding.pry 
+      db.execute(sql, id).map do |row| 
+         
+        self.new(row[0], row[1], row[2])
+      end 
+    
+  end 
+
+
+
 end
